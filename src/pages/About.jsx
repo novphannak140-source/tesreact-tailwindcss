417f83191e5cac93ba473a2539df13f9a508da36
Nov-Phannak <novphannak140@gmail.com>
2025-09-17 16:13:01 +0700

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from "react";
 const About = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 2000); // simulate API call
+  }, []);
   return (
-    <div className="bg-red-600 w-full h-screen text-white font-bold font-serif flex items-center justify-center ">
-      <div className="text-4xl">About</div>
+    <div className="h-screen flex items-center justify-center">
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <h1 className="text-3xl font-bold text-green-600">✅ Data Loaded!</h1>
+      )}
     </div>
   );
 };
